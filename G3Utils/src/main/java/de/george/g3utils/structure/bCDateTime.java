ee4663e9475fe06bb13b1c1a478f581732cacd2c
Georgeto <George.to@gmx.de>
2022-05-28 17:53:19 +0200

    Improve useability of bCDateTime

--- G3Utils/src/main/java/de/george/g3utils/structure/bCDateTime.java
+++ G3Utils/src/main/java/de/george/g3utils/structure/bCDateTime.java
@@ -1,21 +1,41 @@
 package de.george.g3utils.structure;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import de.george.g3utils.io.G3FileReader;
 import de.george.g3utils.io.G3FileWriter;
 import de.george.g3utils.io.G3Serializable;
 
 public class bCDateTime implements G3Serializable {
-	private int lowDateTime;
-	private int highDateTime;
+	public bCDateTime() {}
+
+	public bCDateTime(long filetime) {
+		this.fileTime = filetime;
+	}
+
+	private long fileTime;
 
 	@Override
 	public void read(G3FileReader reader) {
-		lowDateTime = reader.readInt();
-		highDateTime = reader.readInt();
+		fileTime = reader.readLong();
 	}
 
 	@Override
 	public void write(G3FileWriter writer) {
-		writer.writeInt(lowDateTime).writeInt(highDateTime);
+		writer.writeLong(fileTime);
+	}
+
+	private static final Instant FILETIME_ZERO = Instant.parse("1601-01-01T00:00:00Z");
+
+	public static bCDateTime fromInstant(Instant instant) {
+		Duration duration = Duration.between(FILETIME_ZERO, instant);
+		return new bCDateTime(duration.getSeconds() * 10_000_000 + duration.getNano() / 100);
+	}
+
+	public Instant toInstant() {
+		Duration duration = Duration.of(fileTime / 10, ChronoUnit.MICROS).plus(fileTime % 10 * 100, ChronoUnit.NANOS);
+		return FILETIME_ZERO.plus(duration);
 	}
 }
