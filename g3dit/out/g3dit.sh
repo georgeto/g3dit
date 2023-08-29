4d632b6df8a3bca7d4b3abb3e49a4a13088cb182
Georgeto <George.to@gmx.de>
2023-08-29 22:48:35 +0200

    Add startup scripts for Linux

---
+++ g3dit/out/g3dit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+G3DIT_DIR="$(dirname $0)"
+
+java \
+  -XX:+IgnoreUnrecognizedVMOptions \
+  -XX:+UseG1GC \
+  -XX:MinHeapFreeRatio=5 \
+  -XX:MaxHeapFreeRatio=20 \
+  --add-opens=java.desktop/java.awt=ALL-UNNAMED \
+  --add-opens=java.desktop/javax.swing=ALL-UNNAMED \
+  --add-opens=java.base/sun.nio.fs=ALL-UNNAMED \
+  --add-opens=java.prefs/java.util.prefs=ALL-UNNAMED \
+  -Djava.library.path="${G3DIT_DIR}:${G3DIT_DIR}/libs" \
+  -jar "${G3DIT_DIR}/g3dit.jar" \
+  $@ </dev/null &>/dev/null &
