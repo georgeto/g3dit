82b51f861767fa1f28fe55b55c1f074905bcdf9c
Georgeto <George.to@gmx.de>
2024-11-10 12:35:34 +0100

    Set working directory in Linux startup scripts

--- g3dit/out/g3dit.sh
+++ g3dit/out/g3dit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-G3DIT_DIR="$(dirname $0)"
+G3DIT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-java \
+( cd "${G3DIT_DIR}" && java \
   -XX:+IgnoreUnrecognizedVMOptions \
   -XX:+UseG1GC \
   -XX:MinHeapFreeRatio=5 \
@@ -11,6 +11,7 @@ java \
   --add-opens=java.desktop/javax.swing=ALL-UNNAMED \
   --add-opens=java.base/sun.nio.fs=ALL-UNNAMED \
   --add-opens=java.prefs/java.util.prefs=ALL-UNNAMED \
+  -Dg3dit.exefile="${G3DIT_DIR}/g3dit.sh" \
   -Djava.library.path="${G3DIT_DIR}:${G3DIT_DIR}/libs" \
   -jar "${G3DIT_DIR}/g3dit.jar" \
-  $@ </dev/null &>/dev/null &
+  $@ </dev/null &>/dev/null & )
